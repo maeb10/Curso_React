4ae4670b2001fe0ce18efb4e538eb2eea1832d36
Mario Ezqueda <maykrayados@gmail.com>
2024-07-08 12:19:38 -0600

    Expense Control - fixing mobile view of ExpenseDetail component

--- 08-control-gastos-contextapi/src/components/ExpenseDetail.tsx
+++ 08-control-gastos-contextapi/src/components/ExpenseDetail.tsx
@@ -46,12 +46,12 @@ export default function ExpenseDetail({ expense }: ExpenseDetailProps) {
                 leadingActions={leadingActions()}
                 trailingActions={trailingActions()}
             >
-                <div className="bg-white shadow-lg p-5 w-full border-b border-gray-200 flex gap-5 items-center">
+                <div className="bg-white shadow-lg p-5 w-full border-b border-gray-200 flex flex-col md:flex-row gap-5 items-center">
                     <div>
                         <img src={`/icono_${categoryInfo.icon}.svg`} alt="icono gasto" className="w-20" />
                     </div>
 
-                    <div className="flex-1 space-y-2">
+                    <div className="flex-1 space-y-2 text-center md:text-left">
                         <p className="text-sm font-bold uppercase text-slate-500">{categoryInfo.name}</p>
                         <p>{expense.expenseName}</p>
                         <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
